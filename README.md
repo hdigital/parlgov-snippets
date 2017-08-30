6be68fe3f7543f39217369f81d3dd7764709c857
Holger Döring <doering@uni-bremen.de>
2017-08-30 20:26:13 +0200

    adding snippet cabinet compostion by party family

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# parlgov-snippets
-Snippets for dataset generation and analysis with ParlGov – parlgov.org
+# ParlGov snippets
+
+Snippets for dataset generation and analysis with ParlGov – [parlgov.org](http://www.parlgov.org)
+
+ * `cabinet-party-family.R` – cabinet composition by party family (cabinet level and country-year)
---
+++ cabinet-party-family.R
@@ -0,0 +1,69 @@
+# Create dataset of cabinets by party families
+
+library(tidyverse)
+
+rm(list = ls())
+
+
+## Get and read ParlGov cabinet and party data
+
+db_file <- "parlgov-development.db"
+url <- "http://www.parlgov.org/static/data/"
+
+# download ParlGov database if not in local folder
+if( ! db_file %in% list.files()) {
+  download.file(paste0(url, db_file), db_file, mode="wb")
+}
+
+# retrieve data from ParlGov database tables
+con <- DBI::dbConnect(RSQLite::SQLite(), db_file)
+
+cab_raw <- tbl(con, "view_cabinet") %>% collect()
+party_raw <- tbl(con, "view_party") %>% collect()
+ctr_yr_raw <- tbl(con, "viewcalc_country_year_share") %>% collect()
+
+DBI::dbDisconnect(con)
+
+
+## Calculate cabinet level information
+
+cab <- cab_raw %>% 
+  filter(cabinet_party == 1) %>% 
+  left_join(party_raw %>% select(party_id, family = family_name_short))
+
+cab_info <- cab_raw %>% 
+  distinct(country_name_short, country_name, start_date, cabinet_name, caretaker, cabinet_id)
+
+# calculate cabinet level party family shares
+cab_fam <- cab %>% 
+  group_by(cabinet_id, family) %>% 
+  summarise(seats = sum(seats)) %>% 
+  group_by(cabinet_id) %>% 
+  mutate(share = round(seats * 100 / sum(seats), 1),
+         family = if_else(family == "none", "spec", family)) %>% 
+  select(-seats)
+
+# convert from long into wide format with tidyr
+cab_fam_wide <- cab_fam %>% spread(family, share, fill = 0)
+
+cab_long <- cab_info %>% left_join(cab_fam)  
+cab_wide <- cab_info %>% left_join(cab_fam_wide)
+
+write_csv(cab_wide, "cabinet-party-family.csv")
+
+
+## Data in country-year format
+
+ctr_yr <- ctr_yr_raw %>% 
+  filter(id_type == "cabinet") %>% 
+  select(cabinet_id=id, year, year_share = share)
+
+cab_yr_fam <- ctr_yr %>%
+  left_join(cab_fam) %>% 
+  left_join(cab_info %>% select(country_name_short, country_name, cabinet_id)) %>% 
+  group_by(country_name_short, country_name, year, family) %>% 
+  summarise(share = round(sum(share * year_share), 1))
+
+cab_yr_wide <- cab_yr_fam %>% spread(family, share, fill = 0)
+
+write_csv(cab_yr_wide, "cabinet-party-family-year.csv")
