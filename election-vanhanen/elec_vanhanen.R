da52985799df67ec7ae3470930d0fa7225d0597d
lwarode <lukas.warode@gmx.de>
2021-03-11 17:26:14 +0100

    data visualization

--- election-vanhanen/elec_vanhanen.R
+++ election-vanhanen/elec_vanhanen.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(countrycode) # regions
+library(lubridate) # date manipulation
 library(ggsci) # color palette
 
 # set working directory in subfolder
@@ -34,7 +35,13 @@ vanhanen_id <- elec %>%
   # Vanhanen thresholds
   filter(competition > 30 & turnout > 10) %>%
   mutate(index = competition * turnout / 100,
-         region = countrycode(country_name_short, "iso3c", "region23"))
+         # Regions from World Bank Development Indicators 
+         region = countrycode(country_name_short, "iso3c", "region23"),
+         year = lubridate::year(election_date)) 
+
+vanhanen_id_mean <- vanhanen_id %>% 
+  group_by(region, year) %>% 
+  mutate(index_mean = mean(index, na.rm = TRUE)) 
 
 # data visualization ------------------------------------------------------
 
@@ -43,13 +50,51 @@ theme_set(theme_linedraw())
 
 theme_update(strip.background = element_blank(),
              strip.text = element_text(color = "black"),
-             panel.grid.minor = element_blank())
+             panel.grid.minor = element_blank(),
+             legend.position = "top")
 
-# scatterplot
-plot_1 <- ggplot(vanhanen_id, aes(x = election_date, y = index, color = region)) + 
-  geom_point() +
-  # geom_line() +
-  # geom_step() +
-  scale_color_npg()
+# plot all
+plot_all <- ggplot(vanhanen_id, aes(x = year, y = index)) + 
+  geom_point(alpha = 0.5, aes(color = region)) + 
+  geom_line(size = 1, alpha = 0.5, stat = "smooth") +
+  scale_color_nejm(name = "") +
+  scale_x_continuous(breaks = seq(1900, 2020, 20)) + 
+  guides(fill = guide_legend(ncol = 4)) + 
+  labs(title = "Vanhanen Index of Democratization (ID)",
+       x = "Year",
+       y = "Index Value") +
+  theme(text = element_text(family = "Helvetica Light", size = 12),
+        plot.title = element_text(hjust = 0.5, size = 20, face = "bold"))
 
-plot_1  
+# print plot
+plot_all
+
+# saving plot
+ggsave("vanhanen_plot_all.png",
+       plot = plot_all,
+       width = 8,
+       height = 4)
+
+plot_grp <- ggplot(vanhanen_id, aes(x = year, color = region)) +
+  geom_point(aes(y = index), alpha = 0.5, show.legend = FALSE) + 
+  geom_smooth(aes(y = index), se = FALSE) +
+  scale_color_nejm(name = "") +
+  scale_x_continuous(breaks = seq(1900, 2020, 20)) + 
+  lemon::facet_rep_wrap(~ region, ncol = 4, repeat.tick.labels = "all") +
+  guides(fill = guide_legend(ncol = 4)) + 
+  labs(title = "Vanhanen Index of Democratization (ID)",
+       x = "Year",
+       y = "Index Value",
+       caption = "ID is computed as grouped mean (region and year).") +
+  theme(text = element_text(family = "Helvetica Light", size = 12),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        plot.title = element_text(hjust = 0.5, size = 20, face = "bold"))
+
+# print plot
+plot_grp
+
+# saving plot
+ggsave("vanhanen_plot_grouped.png",
+       plot = plot_grp,
+       width = 12,
+       height = 6)
